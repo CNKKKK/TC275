ecee16034d75808dd99d6f0924702e99993fe8a5
CNKKKK <972478710@qq.com>
2019-08-13 16:41:20 +0800

    sts

--- STM32/3YD-USART1(查询)实验/BSP/usart.c
+++ STM32/3YD-USART1(查询)实验/BSP/usart.c
@@ -32,8 +32,10 @@ void USART1_Config(void)
 {	
     GPIO_InitTypeDef GPIO_InitStructure;	
     USART_InitTypeDef USART_InitStructure;  //定义串口初始化结构体
-	  NVIC_InitTypeDef NVIC_InitStructure;
-    RCC_APB2PeriphClockCmd( USART_RCC | USART_GPIO_RCC,ENABLE);//1.串口时钟使能（ RCC_APB2Periph_USART1）以及GPIO使能（RCC_APB2Periph_GPIOA）
+//    NVIC_InitTypeDef NVIC_InitStructure;
+//	  USART_ClockInitTypeDef USART_ClockInitStruct1;//初始化时钟信号
+    
+	RCC_APB2PeriphClockCmd( USART_RCC | USART_GPIO_RCC,ENABLE);//1.串口时钟使能（ RCC_APB2Periph_USART1）以及GPIO使能（RCC_APB2Periph_GPIOA）
 
     /*USART1_TX ->PA9*/			
     GPIO_InitStructure.GPIO_Pin = USART_TX;	       //2.选中串口默认输出管脚         
@@ -46,6 +48,12 @@ void USART1_Config(void)
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
     GPIO_Init(USART_GPIO_PORT, &GPIO_InitStructure);
 	//GPIO的初始化完成
+	//时钟初始化
+//	USART_ClockInitStruct1.USART_Clock=USART_Clock_Disable;
+//	USART_ClockInitStruct1.USART_CPHA=USART_CPHA_2Edge;
+//	USART_ClockInitStruct1.USART_CPOL=USART_CPOL_Low;
+//	USART_ClockInitStruct1.USART_LastBit=USART_LastBit_Disable;
+//	USART_ClockInit(USART1, &USART_ClockInitStruct1);
 	//串口的初始化
     /*串口通讯参数设置*/
     USART_InitStructure.USART_BaudRate = BaudRate; //波特率
@@ -56,24 +64,75 @@ void USART1_Config(void)
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;		//使能接收和发送引脚
 
     USART_Init(USART, &USART_InitStructure);//串口初始化
-		
-		
-		 NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-		 NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=3 ;//中断优先级为3
-		 NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;		//子优先级3
-		 NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ使能
-		 NVIC_Init(&NVIC_InitStructure);	//初始化中断
+		USART_ITConfig(USART1,USART_IT_TC|USART_IT_CTS|USART_IT_ERR|USART_IT_TXE|USART_IT_RXNE,DISABLE);//关闭了中断口
+
+//		 NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+//		 NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=3 ;//中断优先级为3
+//		 NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;		//子优先级3
+//		 NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ使能
+//		 NVIC_Init(&NVIC_InitStructure);	//初始化中断
 
 
-		USART_ClearFlag(USART,USART_FLAG_TC);
-		USART_ITConfig(USART, USART_IT_RXNE, ENABLE);//开启中断
-		USART_ITConfig(USART, USART_IT_TXE, ENABLE);		
+	 USART_ClearFlag(USART,USART_FLAG_TC);
+//		USART_ITConfig(USART, USART_IT_RXNE, ENABLE);//开启中断
+//		USART_ITConfig(USART, USART_IT_TXE, ENABLE);		
     USART_Cmd(USART, ENABLE);//3.使能串口
 		
-		
+		USART_Init(USART,&USART_InitStructure);
 }
 
+void USART2_Config(void)
+{	
+       GPIO_InitTypeDef GPIO_InitStructure2;	
+    USART_InitTypeDef USART_InitStructure2;  //定义串口初始化结构体
+//    NVIC_InitTypeDef NVIC_InitStructure;
+//	  USART_ClockInitTypeDef USART_ClockInitStruct1;//初始化时钟信号
+    
+	RCC_APB1PeriphClockCmd( USART_RCC | USART_GPIO_RCC,ENABLE);//1.串口时钟使能（ RCC_APB2Periph_USART1）以及GPIO使能（RCC_APB2Periph_GPIOA）
 
+    /*USART1_TX ->PA2*/			
+    GPIO_InitStructure2.GPIO_Pin = USART2_TX;	       //2.选中串口默认输出管脚         
+    GPIO_InitStructure2.GPIO_Mode = GPIO_Mode_AF_PP;  // 定义管脚2的模式 推挽
+    GPIO_InitStructure2.GPIO_Speed = GPIO_Speed_50MHz;// 定义输出最大速率 
+    GPIO_Init(USART_GPIO_PORT, &GPIO_InitStructure2);           //调用函数，把结构体参数输入进行初始化		   
+    /*USART1_RX ->PA3*/
+    GPIO_InitStructure2.GPIO_Pin = USART2_RX;	        
+    GPIO_InitStructure2.GPIO_Mode = GPIO_Mode_IN_FLOATING;  // 定义管脚3的模式 浮空
+    GPIO_InitStructure2.GPIO_Speed = GPIO_Speed_50MHz; 
+    GPIO_Init(USART_GPIO_PORT, &GPIO_InitStructure2);
+	//GPIO的初始化完成
+	//时钟初始化
+//	USART_ClockInitStruct1.USART_Clock=USART_Clock_Disable;
+//	USART_ClockInitStruct1.USART_CPHA=USART_CPHA_2Edge;
+//	USART_ClockInitStruct1.USART_CPOL=USART_CPOL_Low;
+//	USART_ClockInitStruct1.USART_LastBit=USART_LastBit_Disable;
+//	USART_ClockInit(USART1, &USART_ClockInitStruct1);
+	//串口的初始化
+    /*串口通讯参数设置*/
+    USART_InitStructure2.USART_BaudRate = BaudRate; //波特率
+    USART_InitStructure2.USART_WordLength = USART_WordLength_8b; //数据位8位
+    USART_InitStructure2.USART_StopBits = USART_StopBits_1;	//停止位1位
+    USART_InitStructure2.USART_Parity = USART_Parity_No;		//校验位 无
+    USART_InitStructure2.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//无流控制
+    USART_InitStructure2.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;		//使能接收和发送引脚
+
+    USART_Init(USART2, &USART_InitStructure2);//串口初始化
+		USART_ITConfig(USART2,USART_IT_TC|USART_IT_CTS|USART_IT_ERR|USART_IT_TXE|USART_IT_RXNE,ENABLE);//关闭了中断口
+
+//		 NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+//		 NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=3 ;//中断优先级为3
+//		 NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;		//子优先级3
+//		 NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ使能
+//		 NVIC_Init(&NVIC_InitStructure);	//初始化中断
+
+
+	 USART_ClearFlag(USART2,USART_FLAG_TC);
+//		USART_ITConfig(USART, USART_IT_RXNE, ENABLE);//开启中断
+//		USART_ITConfig(USART, USART_IT_TXE, ENABLE);		
+    USART_Cmd(USART2, ENABLE);//3.使能串口
+		
+		USART_Init(USART2,&USART_InitStructure2);
+}
 /**
   * @brief  Retargets the C library printf function to the USART.
   * @param  None
--- STM32/3YD-USART1(查询)实验/Project/main.c
+++ STM32/3YD-USART1(查询)实验/Project/main.c
@@ -1,18 +1,13 @@
-/******************** (C) COPYRIGHT  源地工作室 ********************************
+/********************  ********************************
  * 文件名  ：main.c
  * 描述    ：通过串口1打印输出自己输入的字符串；（串口信息为：9600，N,8，1）  
- * 库版本  ：V3.50
- * 作者    ：zhuoyingxingyu
- * 淘宝    ：源地工作室http://vcc-gnd.taobao.com/
- * 论坛地址：极客园地-嵌入式开发论坛http://vcc-gnd.com/
- * 版本更新: 2017-04-20
- * 调试方式：J-Link-OB
+
 **********************************************************************************/	
 
 //头文件
 #include "stm32f10x.h"
 #include "usart.h"
-
+#include "timer.h"
 /**
   * @file   main
   * @brief  Main program.
@@ -22,29 +17,113 @@
 
 
 
-void USART_R_T(char *str)
-{	u8 uart1_get_data; //存储收到的数据
-while(1)
 
+
+//uint16_t TIM_Count;	//定时器计数
+u8 RS232_RX_BUF[200];          //接收缓冲区
+u8 RS232_RX_CNT=0;                        //接收数据长度
+u8 RS232_TX_CNT=0;						//发送数据长度
+//毫秒级别延时
+//void TIM2_delay(u16 time)
+//{
+// 	TIM_Count = time;
+//	while(TIM_Count); 
+//}
+unsigned int crc_cal_value(unsigned char *data_value,unsigned char data_length)//计算CRC的值
 {
-
-if(USART_GetFlagStatus(USART1,USART_IT_RXNE)==SET)
-{ 
-uart1_get_data = USART_ReceiveData(USART1);
-	USART_SendData(USART1,uart1_get_data);
-
+	int i;
+    unsigned int crc_value = 0xffff;//CRC输出为16进制的16位
+    while(data_length--)
+    {
+        crc_value ^= *data_value++;
+        for(i=0;i<8;i++)
+				{
+    if(crc_value&0x0001)
+        crc_value = (crc_value>>1)^0xa001;
+		else
+    crc_value = crc_value>>1;
+				}
+		}			
+	return(crc_value);
 }
 
+void myUSART_Sendbyte(USART_TypeDef* USARTx, uint16_t Data)//给串口发送一位字符
+{
+    while((USARTx->SR&0X40)==0); 
+    USARTx->DR = (Data & (uint16_t)0x01FF);
 }
+
+void myUSART_Sendarr(USART_TypeDef* USARTx, u8 a[] ,uint8_t len)//给串口发送一个数组字符
+{
+	uint8_t i=0;
+	while(i < len )
+		{ 
+			myUSART_Sendbyte( USARTx, a[i]) ;
+			i++;
+		}
 }
+
+void Send_ReadCommandtoInverter(void){		int i;
+/*
+	主机命令：
+	帧头						3.5字节时间
+	ADR 						01H
+	CMD读数据				03H
+	速度起始位高位	10H
+	速度起始位低位	07H
+	寄存器个数高位	00H
+	寄存器个数低位	02H
+	CRC CHK低位			56H
+	CRC CHK高位			CBH
+	帧尾						3.5字符时间
+	*/	
+
+	u8 RS232_TX_BUF[8]={0x01,0x03,0x10,0x07,0x00,0x02};//发送接收速度信号的指令
+	RS232_TX_BUF[6]=crc_cal_value(RS232_TX_BUF,8)&0x00ff;//CRC先把低字节加入到帧中
+	RS232_TX_BUF[7]=(crc_cal_value(RS232_TX_BUF,8)>>8)&0x00ff;//加入高字节
+	printf("Send_ReadCommandtoInverter start");
+		for (i = 0; i<8; i++)
+		printf("%d\n", RS232_TX_BUF[i]);
+	TIM2_delay(3.5*(1000/BaudRate)*10*8);
+	myUSART_Sendarr(USART2,RS232_TX_BUF,8);
+	TIM2_delay(3.5*(1000/BaudRate)*10);
+		printf("Send_ReadCommandtoInverter over");
+}
+void USART2_IRQHandler(void)
+	{//USART2的接收数据
+				printf("USART RECEIVE START");
+	if(USART_GetITStatus(USART2,USART_IT_RXNE)!=RESET)//观察是否接收到数据
+		{
+			USART_ClearITPendingBit(USART2,USART_IT_RXNE);//清除标志位
+			RS232_RX_BUF[RS232_RX_CNT++]=USART_ReceiveData(USART2);
+          if( RS232_RX_CNT==8)
+                RS232_RX_CNT=0;    
+		}
+						printf("USART RECEIVE over");
+	}
+
+void USART1_IRQHandler(void)
+	{	printf("USART SEND START");
+		//static u8 k;
+		USART_ClearFlag(USART1,USART_FLAG_TC);//清除USART1的发送
+		if(USART_GetITStatus(USART1,USART_IT_RXNE)!=Bit_RESET)
+			{
+				USART_SendData(USART1,USART_ReceiveData(USART2));
+						while(USART_GetFlagStatus(USART1,USART_IT_RXNE)!=RESET);
+			}
+			printf("USART SEND over");
+	}
 
 int main(void)
 {
-    USART1_Config();//串口参数配置初始化
+    USART1_Config();//串口1参数配置初始化
+		USART2_Config();//串口2参数配置初始化
     while (1)
     {
-      while (USART_GetFlagStatus(USART1,USART_FLAG_RXNE) == RESET);
-      USART_SendData(USART1,USART_ReceiveData(USART1));
+	printf("Write Function");
+			Send_ReadCommandtoInverter();//发送接收信息的指令
+			USART2_IRQHandler();//USART2接收速度信息
+			USART1_IRQHandler();//UASRT1将UASRT2收到的信息传给PC
     }
 }
 
